35d3a537194dd0278f298f5a6c7324051dc9e339
Juannito Gonzalez <juannito@gmail.com>
2018-10-27 15:25:57 -0300

    Done

--- screens/AuthLoadingScreen.js
+++ screens/AuthLoadingScreen.js
@@ -1,8 +1,12 @@
 import React from 'react'
-import { ActivityIndicator, AsyncStorage, StatusBar, StyleSheet, View } from 'react-native'
-import { getCurrentUser } from '../firebase'
+import { ActivityIndicator, AsyncStorage, StatusBar, StyleSheet, View, TextInput, Button } from 'react-native'
+import { getCurrentUser, db, auth } from '../firebase'
 
 export default class AuthLoadingScreen extends React.Component {
+  state = {
+    name: null,
+  }
+
   constructor(props) {
     super(props)
     this._bootstrapAsync()
@@ -10,8 +14,28 @@ export default class AuthLoadingScreen extends React.Component {
 
   // Fetch the token from storage then navigate to our appropriate place
   _bootstrapAsync = async () => {
+    const { name } = this.state
     const currentUser = await getCurrentUser()
-    this.props.navigation.navigate('App')
+
+    if (name) {
+      this.props.navigation.navigate('App')
+    }
+  }
+
+  nameInset = async () => {
+    const { name } = this.state
+    const userId = await auth.currentUser.uid
+
+    db.collection('users')
+      .doc(userId)
+      .set({
+        name: name,
+      })
+      .then(() => {
+        console.warn('Name')
+      })
+
+    this._bootstrapAsync()
   }
 
   // Render any loading content that you like here
@@ -20,6 +44,11 @@ export default class AuthLoadingScreen extends React.Component {
       <View style={styles.container}>
         <ActivityIndicator />
         <StatusBar barStyle="default" />
+        <TextInput
+          style={{ width: 100, height: 40, borderColor: 'gray', borderWidth: 1 }}
+          onChangeText={text => this.setState({ name: text })}
+        />
+        <Button onPress={this.nameInset} title="Grabar" />
       </View>
     )
   }
